676526ca7e62874ad37da5a8aba6d2b5e0862fc1
ahmed <ahm.gmal@gmail.com>
2021-04-05 23:53:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+
+
+let mainTag = document.querySelector("main#main");
+mainTag.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Ahmed is the champion";
+document.body.appendChild(newHeader);
